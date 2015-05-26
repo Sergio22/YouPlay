cd3138264d4dea06f330d91db61cc19e99de2f72
Sergio22 <sergiomedranocastro@gmail.com>
2015-05-26 17:36:47 +0200

    Agregando funciones

--- wsgi/functions.py
+++ wsgi/functions.py
@@ -7,6 +7,7 @@ def busqueda_g(busqueda,opciones):
 	query_part = {'part':["id,snippet"],'type':opciones,'maxResults':25,'q':busqueda,'key':API_KEY}
 	request_mont = requests.get('https://www.googleapis.com/youtube/v3/search',params=query_part)
 	jresp = json.loads(request_mont.text)
+	jresp["numPag"]= 25
 	return jresp
 
 def	busqueda_next(busqueda,pageid,result_index):
@@ -15,3 +16,5 @@ def	busqueda_next(busqueda,pageid,result_index):
 	jresp = json.loads(request_mont.text)
 	jresp["numPag"]=result_index
 	return jresp
+
+
